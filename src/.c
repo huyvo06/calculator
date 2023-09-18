940aa31ed31abe3a83d1dc8c494dbc8ce6d5c253
huydv <huyducvo2k4@gmail.com>
2023-09-18 13:18:39 +0700

    up2

---
+++ src/.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+	float tienDien,soDau,soCuoi;
+	int bac1=1678,bac2=1734,bac3=2014,bac4=2536,bac5=2834,bac6=2927;
+	float soDien = soCuoi + soDau;
+	 
+	printf("Nhap so dau:");
+	scanf("%f",&soDau);
+	printf("Nhap so cuoi:");
+	scanf("%f",&soCuoi);
+	
+	printf("%.2f",soDien);
+	 
+} 
